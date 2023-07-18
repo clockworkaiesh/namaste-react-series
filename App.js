3e2e049315e04ca08ffcc669f6947024a63ea2de
catlady238 <ayesha@hashpy.com>
2023-07-18 16:29:37 +0500

    episode 1

---
+++ App.js
@@ -0,0 +1,6 @@
+const heading = React.createElement("h1", {className: 'title'}, "Hello from core React")
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(heading)
+
